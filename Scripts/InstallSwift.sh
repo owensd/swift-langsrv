563edd8b0971266f1e4fa24182b485c9d0bdabe1
David Owens II <david@owensd.io>
2017-04-28 16:28:26 -0700

    Fixed the download path.

--- Scripts/InstallSwift.sh
+++ Scripts/InstallSwift.sh
@@ -6,13 +6,13 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 	DIR="$(pwd)"
 	cd ..
 	export SWIFT_VERSION=swift-3.1.1-RELEASE
-    wget https://swift.org/builds/${SWIFT_VERSION}/ubuntu1404/${SWIFT_VERSION}/${SWIFT_VERSION}-ubuntu14.04.tar.gz
+    wget https://swift.org/builds/swift-3.1.1-release/ubuntu1404/${SWIFT_VERSION}/${SWIFT_VERSION}-ubuntu14.04.tar.gz
 	tar xzf $SWIFT_VERSION-ubuntu14.04.tar.gz
 	export PATH="${PWD}/${SWIFT_VERSION}-ubuntu14.04/usr/bin:${PATH}"
 	cd "$DIR"
 else
 	export SWIFT_VERSION=swift-3.1.1-RELEASE
-    curl -O https://swift.org/builds/${SWIFT_VERSION}/xcode/${SWIFT_VERSION}/${SWIFT_VERSION}-osx.pkg
+    curl -O https://swift.org/builds/swift-3.1.1-release/xcode/${SWIFT_VERSION}/${SWIFT_VERSION}-osx.pkg
 	sudo installer -pkg ${SWIFT_VERSION}-osx.pkg -target /
 	export TOOLCHAINS=swift
 fi
