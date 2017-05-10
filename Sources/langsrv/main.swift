9c0e7e0fc04a32bec3e86b0f6eb2601e1ef1ddc3
David Owens II <david@owensd.io>
2017-05-10 09:42:11 -0700

    Re-enabled the main.swift content.

--- Sources/langsrv/main.swift
+++ Sources/langsrv/main.swift
@@ -1,13 +1,13 @@
-// /*
-//  * Copyright (c) Kiad Studios, LLC. All rights reserved.
-//  * Licensed under the MIT License. See License in the project root for license information.
-//  */
+/*
+ * Copyright (c) Kiad Studios, LLC. All rights reserved.
+ * Licensed under the MIT License. See License in the project root for license information.
+ */
 
-// import langsrvlib
+import langsrvlib
 
-// // Currently, the only supported language server protocol is `vscode-jsonrpc`.
+// Currently, the only supported language server protocol is `vscode-jsonrpc`.
 
-// let standardInput = StandardInputMessageSource()
-// let jsonrpc = LanguageServerProtocol()
-// let langsrv = SwiftLanguageServer()
-// langsrv.run(source: standardInput, transport: jsonrpc)
+let standardInput = StandardInputMessageSource()
+let jsonrpc = LanguageServerProtocol()
+let langsrv = SwiftLanguageServer()
+langsrv.run(source: standardInput, transport: jsonrpc)
