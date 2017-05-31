582328de3c93f55888a0a7138eec50ff72bdb399
David Owens II <david@owensd.io>
2017-05-30 22:05:25 -0700

    Remove the 'VersionInfo' dep.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "swift-langsrv",
     targets: [
-        Target(name: "langsrv", dependencies: ["langsrvlib", "VersionInfo"])
+        Target(name: "langsrv", dependencies: ["langsrvlib"])
     ],
     dependencies: [
         .Package(url: "https://github.com/owensd/json-swift.git", majorVersion: 2, minor: 0),
