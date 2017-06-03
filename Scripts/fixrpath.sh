a7c1b95fc30e85eaa058867090724b2be3df982c
David Owens II <david@owensd.io>
2017-06-03 16:40:37 -0700

    Fixed rpath to be executable_path

--- Scripts/fixrpath.sh
+++ Scripts/fixrpath.sh
@@ -4,4 +4,4 @@ ProjectRoot=$(pwd)
 BuildPath=$ProjectRoot/.build/$1
 
 echo "Changing rpath of libsourcekitd.dylib"
-install_name_tool -change "$BuildPath/libsourcekitd.dylib" "@rpath/libsourcekitd.dylib" $BuildPath/langsrv
+install_name_tool -change "$BuildPath/libsourcekitd.dylib" "@executable_path/libsourcekitd.dylib" $BuildPath/langsrv
